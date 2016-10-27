3015b3198404f386f512c9ff32182f848211763b
wr9 <asikim0@fesb.hr>
2016-10-27 12:54:45 +0200

    init

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Navbar } from './components/Navbar';
 
 @NgModule({
- imports: [ BrowserModule ],
- declarations: [ AppComponent ],
+ imports: [ BrowserModule, NgbModule.forRoot()],
+ declarations: [ AppComponent, Navbar ],
  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
