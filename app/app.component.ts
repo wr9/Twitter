3c570590bff18ed59c3f903eadfcf3e6b5767c6e
wr9 <asikim0@fesb.hr>
2016-11-02 23:25:56 +0100

    navbar, login form, register form(some)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
  selector: 'my-app',
- template: '<h1>My First Angular App</h1> <navbar></navbar>'
+ template: '<navbar></navbar><login-form></login-form><register-form></register-form>'
 })
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/navbar/navbar';
+import { LoginForm } from './components/loginForm/loginForm';
+import { RegisterForm } from './components/registerForm/registerForm';
 
 @NgModule({
  imports: [ BrowserModule, NgbModule.forRoot()],
- declarations: [ AppComponent, Navbar ],
+ declarations: [ AppComponent, Navbar, LoginForm,RegisterForm ],
  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
