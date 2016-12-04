5aa77a1d6d4d6e7901af98c97b71d76d008f33fc
viki <irebic00@fesb.hr>
2016-12-04 22:38:10 +0100

    Sve gotovo

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -5,7 +5,6 @@ import { RegisterForm } from './components/registerForm/registerForm';
 import { EditProfile } from './components/editProfile/editProfile';
 import { MainPage } from './components/mainPage/mainPage';
 import { SearchForm } from './components/searchForm/searchForm';
-import { UserCover } from './components/userCover/userCover';
 
 export const routes: Routes = [
   {
@@ -18,7 +17,6 @@ export const routes: Routes = [
   { path: 'edit-profile', component: EditProfile },
   { path: 'main-page', component: MainPage },
   { path: 'search-form', component: SearchForm },
-  { path: 'user-cover', component: UserCover }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
